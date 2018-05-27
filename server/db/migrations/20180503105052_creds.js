08e2cb47499f77a4659f648e23da55e19d77c58f
Frank <xuzy213@gmail.com>
2018-05-27 22:50:04 +1200

    fix knex table issue

--- server/db/migrations/20180503105052_creds.js
+++ server/db/migrations/20180503105052_creds.js
@@ -7,5 +7,5 @@ exports.up = (knex, Promise) => {
 }
 
 exports.down = (knex, Promise) => {
-  return knex.dropTable('creds')
+  return knex.schema.dropTable('creds')
 }
--- server/db/migrations/20180503105145_users.js
+++ server/db/migrations/20180503105145_users.js
@@ -8,5 +8,5 @@ exports.up = (knex, Promise) => {
 }
 
 exports.down = (knex, Promise) => {
-  return knex.dropTable('users')
+  return knex.schema.dropTable('users')
 }
